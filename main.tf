c60e08196152c2bc7bdbc48a8586ea8727975d43
andrew.clure@irwinmitchell.com <andrew@andrews-air.home>
2021-05-18 14:38:04 +0100

    set minimum provider for vnet deployment module

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+# Minimum azure provider to use
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.54.0"
+    }
+  }
+}
+
+
 locals {
   module_tag = {
     "module" = basename(abspath(path.module))
